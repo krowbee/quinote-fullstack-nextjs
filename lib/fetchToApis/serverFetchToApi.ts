6db48f5afe89fec6266badb142c6f02815c90495
krowbee <skoresplay@gmail.com>
2025-12-11 15:41:28 +0200

    fix: added getBaseUrl in serverFetchToApi

--- lib/fetchToApis/serverFetchToApi.ts
+++ lib/fetchToApis/serverFetchToApi.ts
@@ -1,19 +1,20 @@
 "use server";
-import { cookies } from "next/headers";
+import { cookies, headers } from "next/headers";
+
+export async function getBaseUrl(): Promise<string> {
+  const h = await headers();
+  const host = h.get("host");
+  return `${
+    process.env.NODE_ENV === "production" ? "https://" : "http://"
+  }${host}`;
+}
 
 export async function serverFetchToApi(path: string) {
   const cookieStore = (await cookies()).toString();
-
-  const res = await fetch(
-    `https://${
-      process.env.NODE_ENV === "production"
-        ? process.env.VERCEL_URL
-        : "localhost:3000"
-    }${path}`,
-    {
-      headers: { Cookie: cookieStore },
-      cache: "no-store",
-    }
-  );
+  const baseUrl = await getBaseUrl();
+  const res = await fetch(`${baseUrl}${path}`, {
+    headers: { Cookie: cookieStore },
+    cache: "no-store",
+  });
   return res;
 }
